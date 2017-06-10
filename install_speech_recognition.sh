2f8d02ab198924b563d0bc036312cefe458118c2
Ivo Meixner <natiwastaken@gmail.com>
2017-06-10 19:57:48 +0200

    Made all commands executed with sudo

--- install_speech_recognition.sh
+++ install_speech_recognition.sh
@@ -3,7 +3,7 @@ set -e
 sudo apt update
 sudo apt install python3
 sudo apt install python3-pip
-pip3 install --upgrade pip
+sudo pip3 install --upgrade pip
 sudo pip3 install SpeechRecognition
 sudo apt install portaudio19-dev python-all-dev python3-all-dev
 sudo pip3 install pyaudio
