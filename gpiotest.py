e57bcaa9a0ef6532c8015ea007ce92ad6b3dbae7
Ivo Meixner <ivo.meixner@se7en.cz>
2017-07-07 01:07:38 +0200

    Created gpiotest.py | Not yet tested

---
+++ gpiotest.py
@@ -0,0 +1,32 @@
+"""
+This script is made for the purpose of testing the GPIO
+pins on a Raspberry Pi 3 Model B.
+"""
+
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO! This is probably because you need superuser privileges. \
+        You can achieve this by using 'sudo' to run your script")
+
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+def write(channel, state):
+    """This function sets a GPIO pin to a specified state."""
+    GPIO.setup(channel, GPIO.OUT)
+    GPIO.output(channel, state)
+
+def read(channel):
+    """This function returns the state of a specified GPIO pin."""
+    GPIO.setup(channel, GPIO.IN)
+    return GPIO.input(channel)
+
+while True:
+    write(18, 1)
+    time.sleep(0.5)
+    write(18, 0)
+    time.sleep(0.5)
+
+GPIO.cleanup()
